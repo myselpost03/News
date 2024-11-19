7d1e4cb0daf9249a6bf7b910eeecc4e4ab54b453
anuj <anujr6881@gmail.com>
2024-11-19 22:58:25 +0530

    all fixes

--- Client/src/Components/config.js
+++ Client/src/Components/config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://news-1fjk.onrender.com/api";
+export const BASE_URL = "https://news-mbis.onrender.com/api";
 
-//https://news-1fjk.onrender.com
+//https://news-mbis.onrender.com
 //http://localhost:5000
 //https://characteristic-caribou-simple-news-8f606f43.koyeb.app/api
