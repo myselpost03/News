57bbb0b0b5bfc0c020df7ce869feba850daf29f4
anuj <anujr6881@gmail.com>
2024-11-19 22:04:36 +0530

    change

--- Client/src/Components/config.js
+++ Client/src/Components/config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://news-lj4i.onrender.com/api";
+export const BASE_URL = "https://news-1fjk.onrender.com/api";
 
-//https://news-lj4i.onrender.com
+//https://news-1fjk.onrender.com
 //http://localhost:5000
 //https://characteristic-caribou-simple-news-8f606f43.koyeb.app/api
